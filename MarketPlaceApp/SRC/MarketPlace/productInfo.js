b340e9dbca2782e0c5f66c2902463563a5064f67
JGreyScales <77678089+JGreyScales@users.noreply.github.com>
2025-10-02 20:23:19 -0400

    setting up modals on productInfo page

--- MarketPlaceApp/SRC/MarketPlace/productInfo.js
+++ MarketPlaceApp/SRC/MarketPlace/productInfo.js
@@ -11,7 +11,34 @@ export default function ProductInfo({navigation}) {
     const [error, setError] = useState(null);
     const [loading, setLoading] = useState(true);
 
+    const [purchaseModal, setPurchaseModal] = useState(false);
+    const [reviewModal, setReviewModal] = useState(false);
+
     const [productContent, setProductContent] = useState(null)
+    const [userWalletAmount, setUserWalletAmount] = useState(0.00);
+    const [userRatingOfProduct, setUserRatingOfProduct] = useState(0);
+
+    const grabUserWallet = async () => {
+        try{
+            const sessionToken = await SessionStorage.getItem('@sessionKey');
+            const response = await fetch('http://localhost:3000/user/fund', {
+                method: 'GET',
+                headers: {
+                    Authorization: sessionToken
+                }
+            })
+
+            if (!response.ok) {
+                setError('Failed fetching user wallet')
+                return
+            }
+            const data = parseFloat((await response.json()).userFundsAmount).toFixed(2)
+            setUserWalletAmount(data)
+
+        } catch (error){
+            setError('Fetch error:' + error.message);
+        }
+    }
 
     const grabProductDetails = async () => {
         try{
@@ -37,8 +64,17 @@ export default function ProductInfo({navigation}) {
        
     }
 
+    const onPurchase = () => {
+
+    }
+
+    const onSubmit = () => {
+
+    }
+
     useEffect(() => {
         grabProductDetails()
+        grabUserWallet()
     }, [route.params]);
 
     if (error) {
@@ -59,48 +95,106 @@ export default function ProductInfo({navigation}) {
       }
 
     return (
-        <View style={GlobalStyles.container}>
-            <CustomButton text="Back" onPress={() => navigation.goBack()} />
-            <CustomButton text="Purchase" onPress={1} />
-            <CustomButton text="Review" onPress={1} />
+        <>
+            <View style={GlobalStyles.container}>
+                <CustomButton text="Back" onPress={() => navigation.goBack()} />
+                <CustomButton text="Purchase" onPress={() => setPurchaseModal(true)} />
+                <CustomButton text="Review" onPress={() => setReviewModal(true)} />
 
-            <ScrollView style={GlobalStyles.productDetailContainer}>
-            <View style={GlobalStyles.productDetailImageContainer}>
-            <Image
-                source={{ uri: productContent.productImage ? `data:image/png;base64,${productContent.productImage}` : undefined }}
-                style={GlobalStyles.productDetailImage}
-            />
-            </View>
-    
-            <View style={GlobalStyles.productDetailInfo}>
-            <Text style={GlobalStyles.productDetailTitle}>{productContent.productName}</Text>
-            <TouchableOpacity onPress={() => {}}>
-                <Text style={GlobalStyles.productDetailSeller}>Sold by: {productContent.storepageName}</Text>
-            </TouchableOpacity>
-            <Text style={GlobalStyles.productDetailPrice}>${parseFloat(productContent.productPrice).toFixed(2)}</Text>
-            <Text style={GlobalStyles.productDetailRating}>⭐ {parseFloat(productContent.productRating).toFixed(1)}</Text>
-    
-            <Text style={GlobalStyles.productDetailDescription}>{productContent.productBio}</Text>
-    
-            <View style={{ marginTop: 20 }}>
-                <Text style={GlobalStyles.sectionTitle}>Tags</Text>
-                <View style={GlobalStyles.interestsList}>
-                {productContent.interests.map((interest, idx) => (
-                    <View key={idx} style={GlobalStyles.interestChip}>
-                    <Text style={GlobalStyles.interestText}>{interest}</Text>
-                    </View>
-                ))}
+                <ScrollView style={GlobalStyles.productDetailContainer}>
+                <View style={GlobalStyles.productDetailImageContainer}>
+                <Image
+                    source={{ uri: productContent.productImage ? `data:image/png;base64,${productContent.productImage}` : undefined }}
+                    style={GlobalStyles.productDetailImage}
+                />
                 </View>
+        
+                <View style={GlobalStyles.productDetailInfo}>
+                <Text style={GlobalStyles.productDetailTitle}>{productContent.productName}</Text>
+                <TouchableOpacity onPress={() => {}}>
+                    <Text style={GlobalStyles.productDetailSeller}>Sold by: {productContent.storepageName}</Text>
+                </TouchableOpacity>
+                <Text style={GlobalStyles.productDetailPrice}>${parseFloat(productContent.productPrice).toFixed(2)}</Text>
+                <Text style={GlobalStyles.productDetailRating}>⭐ {parseFloat(productContent.productRating).toFixed(1)}</Text>
+        
+                <Text style={GlobalStyles.productDetailDescription}>{productContent.productBio}</Text>
+        
+                <View style={{ marginTop: 20 }}>
+                    <Text style={GlobalStyles.sectionTitle}>Tags</Text>
+                    <View style={GlobalStyles.interestsList}>
+                    {productContent.interests.map((interest, idx) => (
+                        <View key={idx} style={GlobalStyles.interestChip}>
+                        <Text style={GlobalStyles.interestText}>{interest}</Text>
+                        </View>
+                    ))}
+                    </View>
+                </View>
+        
+                <Text style={{ marginTop: 20, fontSize: 12, color: '#999' }}>
+                    Created: {new Date(productContent.createdAT).toLocaleDateString()}
+                </Text>
+                <Text style={{ fontSize: 12, color: '#999' }}>
+                    Updated: {new Date(productContent.updatedAT).toLocaleDateString()}
+                </Text>
+                </View>
+            </ScrollView>
+        </View>
+
+
+         {/* purchase Product Modal */}
+         <Modal visible={purchaseModal} transparent animationType="slide">
+          <View style={GlobalStyles.modalWrapper}>
+            <View style={GlobalStyles.modalContainer}>
+                <Text style={GlobalStyles.inputLabel}>Amount In Account:{userWalletAmount}</Text>
+                <Text style={GlobalStyles.inputLabel}>Product Price:{parseFloat(productContent.productPrice).toFixed(2)}</Text>
+                <TouchableOpacity
+                    style={[GlobalStyles.button, GlobalStyles.modalButtonCancel]}
+                    onPress={() => {setPurchaseModal(false)}}
+                >
+                    <Text style={GlobalStyles.modalButtonTextCancel}>Cancel</Text>
+                </TouchableOpacity>
+                <TouchableOpacity
+                    style={[GlobalStyles.button, GlobalStyles.modalButtonSave]}
+                    onPress={onPurchase}
+                >
+                    <Text style={GlobalStyles.buttonText}>Purchase</Text>
+                </TouchableOpacity>
             </View>
-    
-            <Text style={{ marginTop: 20, fontSize: 12, color: '#999' }}>
-                Created: {new Date(productContent.createdAT).toLocaleDateString()}
-            </Text>
-            <Text style={{ fontSize: 12, color: '#999' }}>
-                Updated: {new Date(productContent.updatedAT).toLocaleDateString()}
-            </Text>
+          </View>
+        </Modal>
+
+        {/* purchase Product Modal */}
+        <Modal visible={reviewModal} transparent animationType="slide">
+          <View style={GlobalStyles.modalWrapper}>
+            <View style={GlobalStyles.modalContainer}>
+            <Text style={GlobalStyles.inputLabel}>Enter Your Rating 1-5</Text>
+                <TextInput
+                    style={GlobalStyles.input}
+                    keyboardType="numeric"
+                    value={userRatingOfProduct}
+                    onChangeText={(text) => {
+                        const decimalText = text.replace(/[^0-9.]/g, '');
+                        setUserRatingOfProduct(decimalText);
+                    }}
+                    placeholder="Enter Rating 1-5"
+                    />
+                <TouchableOpacity
+                    style={[GlobalStyles.button, GlobalStyles.modalButtonCancel]}
+                    onPress={() => {setReviewModal(false)}}
+                >
+                    <Text style={GlobalStyles.modalButtonTextCancel}>Cancel</Text>
+                </TouchableOpacity>
+                <TouchableOpacity
+                    style={[GlobalStyles.button, GlobalStyles.modalButtonSave]}
+                    onPress={onSubmit}
+                >
+                    <Text style={GlobalStyles.buttonText}>Submit</Text>
+                </TouchableOpacity>
             </View>
-        </ScrollView>
-      </View>
+          </View>
+        </Modal>
+
+
+      </>
     );
 }
